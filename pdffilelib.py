efe7abe2a1bff2dee5dfff4b187d37158416e950
jmorwick <jbmorwick@gmail.com>
2017-07-17 18:04:39 -0500

    added function for saving pdf files to the store

--- pdffilelib.py
+++ pdffilelib.py
@@ -8,8 +8,8 @@ def save_pdf_file(directory, data, pub_key = None):
     directory: system path of the datastore
     data: binary pdf data for the file
     key: public encryption key to encode the file with"""    
-    datahash = hashlib.sha256(data).hexdigest()
-    fname = directory + '/' + datahash
+    hashhex = hashlib.sha256(data).hexdigest()
+    fname = directory + '/' + hashhex
     if pub_key is not None:
         data = rsa.encrypt(data, pub_key)
         fname = fname + '-' + str(pub_key.n) + '-' + str(pub_key.e)
@@ -18,3 +18,22 @@ def save_pdf_file(directory, data, pub_key = None):
     f.write(data)
     f.close()
     
+def load_pdf_file(directory, hashhex, priv_key = None):
+    """load binary pdf data from the store
+
+    parameters:
+    directory: system path of the datastore
+    hashex: a sha256 hash of the unencrypted file encoded as a hex string
+    priv_key: the decryption key (if the file is encrypted)
+    """
+    fname = directory + '/' + hashhex
+    if priv_key is not None:
+        fname = fname + '-' + str(priv_key.n) + '-' + str(priv_key.e)
+    fname = fname + '.pdf'
+    f = open(fname, 'rb')
+    data = f.read()
+    if priv_key is not None:
+        data = rsa.decrypt(data, priv_key)
+    return data
+    
+        
